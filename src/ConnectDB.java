e72886022340307e3cb05750b25097265d4c6f7c
Prasad7659 <pkrupavara@gmail.com>
2025-07-04 11:47:04 +0530

    Updated Driver Name

--- src/ConnectDB.java
+++ src/ConnectDB.java
@@ -1,7 +1,7 @@
 package src;
 
 public class ConnectDB {
-    String Driver = "com.mysql.jdbc.Driver";
+    String Driver = "com.mysql.cj.jdbc.Driver";
     String url = "jdbc:mysql://localhost:3306/eventmanagementdb";
     String user = "root";
     String pass = "Prasad@223";
